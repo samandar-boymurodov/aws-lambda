3b32f707b8c97f6c8d18b8c8e49a24ea7cbf7b18
Samandar Boymurodov <boymurodovuzcoder@gmail.com>
2023-10-21 23:05:32 +0500

    Integrated database to getProductsList lambda

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,11 +1,39 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import { mockProducts } from '@libs/mock-products'
 
+const awsRegion = 'eu-west-1';
+
+const dynamoDbClient = new DynamoDBClient({
+  region: awsRegion
+});
+
+const dynamoDbDocumentClient = DynamoDBDocumentClient.from(dynamoDbClient);
 
 const getProductsList: ValidatedEventAPIGatewayProxyEvent<{}> = async () => {
-  return formatJSONResponse(mockProducts);
+  const productData = await dynamoDbDocumentClient.send(
+      new ScanCommand({ TableName: 'Products' })
+  );
+
+  const stockData = await dynamoDbDocumentClient.send(
+      new ScanCommand({ TableName: 'Stocks' })
+  );
+
+  const combinedData = productData.Items?.map((product) => {
+    const stockItem = stockData.Items?.find(
+        (stock) => stock.product_id === product.id
+    );
+
+    return {
+      ...product,
+      count: stockItem?.count,
+    };
+  });
+
+  return formatJSONResponse(combinedData);
 };
 
 export const main = middyfy(getProductsList);
